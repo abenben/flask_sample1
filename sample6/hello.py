fd2cce3e4753064b60450497ec5bea875bbf7ec5
abenben <abenbenben@gmail.com>
2022-01-01 16:15:58 +0900

    追加

--- sample6/hello.py
+++ sample6/hello.py
@@ -1,18 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='.',static_url_path="")
+
+
+# @app.route('/')
+# def hello():
+#     name = "Hello World"
+#     return name
 
 
 @app.route('/')
-def hello():
-    name = "Hello World"
-    return name
-
-
-@app.route('/good')
 def good():
     name = "Good"
-    return name
+    # return name
+    return render_template('hello.html', title='flask test', name=name)
 
 
 if __name__ == '__main__':
